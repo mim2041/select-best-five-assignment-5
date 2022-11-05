4ba4f94225d70e306b38440bc44cbb53510ebc65
mim2041 <you@example.com>
2022-11-05 20:21:12 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-document.getElementById('select-btns').addEventListener('click',function(event){
+document.getElementById('select-player').addEventListener('click',function(event){
     const selectButton = document.getElementById('btn-select');
     const playerList = document.getElementById('player-list');
     const li = document.createElement('li');
 
+    li.innerText = playerNames('messi');
+    li.innerText = playerNames('neymar');
+    li.innerText = playerNames('mbappe');
+    li.innerText = playerNames('vitor');
+    li.innerText = playerNames('ramos');
+    li.innerText = playerNames('renato');
+    li.innerText = playerNames('messi1');
+    li.innerText = playerNames('neymar1');
+    li.innerText = playerNames('mbappe1');
+    li.innerText = playerNames('vitor1');
+    li.innerText = playerNames('ramos1');
+    li.innerText = playerNames('renato1');
+
+    playerList.appendChild(li);
 })
+
+function playerNames(playerId){
+    const playerIdField = document.getElementById(playerId);
+    const playerNames = playerIdField.innerText;
+    return playerNames;
+}
