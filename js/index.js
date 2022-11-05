f2f75064f0f77547bc4ed0cda0a02a072566fc3c
mim2041 <you@example.com>
2022-11-05 19:12:59 +0600

    project processing

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('select-btns').addEventListener('click',function(event){
+    const selectButton = document.getElementById('btn-select');
+    const playerList = document.getElementById('player-list');
+    const li = document.createElement('li');
+
+})
